2e7216e925d37ee141568b22449916ca8d3bf3b5
marc_doblefilo <rodriguezmoreno.marcos@gmail.com>
2020-09-22 23:59:28 +0200

    Added exercise 3

--- pr1/exercise3.c
+++ pr1/exercise3.c
@@ -4,8 +4,9 @@
 #include <unistd.h>
 #include <errno.h>
 #include <sys/wait.h>
+#include <string.h>
 
-int main(int arc, char** argv) {
+int main(int argc, char** argv) {
 
     pid_t pid, child_pid;
     int status;
@@ -13,7 +14,52 @@ int main(int arc, char** argv) {
     pid = fork();
     if(pid == 0) {
         printf("This is the child %d with pid %d, which father's %d\n", status, getpid(), getppid());
-           
+        char* arg1[] = {argv[1], NULL};
+        execv("/usr/bin/mate-calculator", arg1);
+        exit(EXIT_SUCCESS);
+    }
+    else if(pid == -1) {
+        perror("Error creating a child process\n");
+        exit(EXIT_FAILURE);
     }
 
+    pid = fork();
+    if(pid == 0) {
+        printf("This is the child %d with pid %d, which father's %d\n", status, getpid(), getppid());
+        char* arg2[] = {argv[2]};
+        for(int i = 3; i < argc; i++){
+           arg2[i-2] = argv[i];
+        }
+        arg2[argc-2] = NULL;
+        execv("/usr/bin/pluma", arg2);
+        exit(EXIT_SUCCESS);
+    }
+    else if(pid == -1) {
+        perror("Error creating a child process\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    for (int j = 0; j < 2; j++) {
+        child_pid = wait(&status);
+
+        if(child_pid > 0) {
+            if (WIFEXITED(status)){
+                printf("Child %d killed (signal %d)\n", child_pid, WTERMSIG(status));
+            }
+            else if (WIFSIGNALED(status)){
+                printf("Child %d killed (signal %d)\n", child_pid, WTERMSIG(status));
+            }
+            else if (WIFSTOPPED(status)){
+                printf("Child %d stopped (signal %d)\n", child_pid, WSTOPSIG(status));
+            }
+        }
+        else {
+                printf("Error invoking wait or the call was interrupted\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+
+
+    exit(EXIT_SUCCESS);
 }
