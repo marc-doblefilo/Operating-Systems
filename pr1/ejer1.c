9998c50731f72865803783e2883a3f2ab24604ea
marc_doblefilo <rodriguezmoreno.marcos@gmail.com>
2020-09-22 15:57:22 +0200

    Added Practice1

---
+++ pr1/ejer1.c
@@ -0,0 +1,47 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/wait.h>
+
+int main(int argc, char** argv) {
+
+    pid_t pid, pid_child;
+    int status;
+    int PROCESS = atoi(argv[1]);
+
+    for (int i = 0; i < PROCESS; i++) {
+        pid = fork();
+        if (pid == 0) {
+            printf("This is the child %d with pid %d, which father's %d\n", status, getpid(), getppid());
+            exit(EXIT_SUCCESS);
+        }
+        else if (pid == -1) {
+            perror("Error creating a child");
+            exit(EXIT_FAILURE);
+        }
+    }
+    for (int j = 0; j < PROCESS; j++) {
+        pid_child = wait(&status);
+
+        if(pid_child > 0) {
+            if (WIFEXITED(status)){
+                printf("Child %d killed (signal %d)\n", pid_child, WTERMSIG(status));
+            }
+            else if (WIFSIGNALED(status)){
+                printf("Child %d killed (signal %d)\n", pid_child, WTERMSIG(status));
+            }
+            else if (WIFSTOPPED(status)){
+                printf("Child %d stopped (signal %d)\n", pid_child, WSTOPSIG(status));
+            }
+        }
+        else {
+                printf("Error invoking wait or the call was interrupted\n");
+                exit(EXIT_FAILURE);
+        }
+    }
+
+    exit(EXIT_SUCCESS);
+
+} 
