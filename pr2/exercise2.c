2198ffef1b9a9474a5dbe905f7f7f973c4e3e302
mark-doblefilo <i92romom@uco.es>
2020-10-19 19:53:49 +0200

    Added ex1 and ex2 from practice 2

---
+++ pr2/exercise2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+
+void count_lines(void* filenames) {
+    int * number_lines = calloc(1, sizeof(int));
+    char * filename = filenames;
+    printf("%p \n", filenames);
+    FILE * file_stream = fopen(filename, "r");
+    if (file_stream == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    while(!feof(file_stream)){
+    char ch = fgetc(file_stream);
+    if (ch == '\n'){
+      *number_lines = *number_lines + 1;
+    }
+  }
+  printf("%i\n",*number_lines );
+  pthread_exit( (void *) number_lines);
+}
+
+int main(int argc, char **argv){
+    int total_lines;
+
+    pthread_t *threads = calloc(argc -1, sizeof(pthread_t));
+    if(threads == NULL){
+        perror("Error allocating memory for threads array.");
+        exit(EXIT_FAILURE);
+    }
+
+    int **lines_array = calloc(argc -1, sizeof(int*));
+    if(lines_array == NULL) {
+        perror("Error allocating memory for lines array.");
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i = 1; i < argc; i++) {
+        pthread_create(&threads[i], NULL, (void*) count_lines, (void*) argv[i]);
+    }
+
+    for(int i = 1; i < argc; i++) {
+        pthread_join(threads[i], (void **) &lines_array[i]);
+    }
+
+    for(int i = 1; i < argc; i++) {
+        total_lines = total_lines + *lines_array[i];
+    }
+
+    printf("Number of lines: %d\n", total_lines);
+    exit(EXIT_SUCCESS);
+}
